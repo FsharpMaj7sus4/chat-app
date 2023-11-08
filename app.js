3e6c6f957c9a42335fd0dd444779a108d4ed75ec
Farid Athary <f.athary@gmail.com>
2023-11-08 15:45:45 +0330

    fix

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/userRoutes');
-const authRouter = require('./routes/authRoutes');
-const globalErrorHandler = require('./utils/globalErrorHandler');
-const dotenv = require('dotenv')
+const createError = require("http-errors")
+const express = require("express")
+const path = require("path")
+const cookieParser = require("cookie-parser")
+const logger = require("morgan")
+const dotenv = require("dotenv")
 
 dotenv.config()
+const indexRouter = require("./routes/index")
+const usersRouter = require("./routes/userRoutes")
+const authRouter = require("./routes/authRoutes")
+const globalErrorHandler = require("./utils/globalErrorHandler")
 
-const app = express();
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
 
-app.use('/', indexRouter);
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/users', usersRouter);
+app.use("/", indexRouter)
+app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/users", usersRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
 // app.use(function(err, req, res, next) {
@@ -44,5 +44,4 @@ app.use(function(req, res, next) {
 // });
 app.use(globalErrorHandler)
 
-
-module.exports = app;
+module.exports = app
